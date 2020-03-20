9fc6b9cddeb2b309d540b7f906c6f11104a93f44
swapnil13cs56 <swapnil13sc56@gmail.com>
2020-03-21 00:36:24 +0530

    changed the files to php files

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+        <div class="container">
+          <span><a href="https://www.instagram.com/soup_lazy/" target="_blank" class="social"><i class="fab fa-instagram fa-2x"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://medium.com/@swapnilkr" target="_blank" class="social"><i class="fab fa-medium fa-2x"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/swapnil.lazybone" target="_blank" class="social"><i class="fab fa-facebook fa-2x"></i></a></span>
+
+         
+        </div>
+      </footer>
+      
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+  </body>
+</html>
